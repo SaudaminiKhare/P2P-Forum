7b5080b12801d53141e34d3d50e7e8dd8b8f40e1
swetha242 <sswetha.242@gmail.com>
2018-10-14 19:03:57 +0530

    Notes

--- notes.py
+++ notes.py
@@ -7,6 +7,49 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/SEdb"
 mongo = PyMongo(app)
 
+#sample JSONs
+'''
+{
+	"_id" : ObjectId("5bc0de4d7495c3f50f6c5bd6"),
+	"tag" : "ML",
+	"course" : 1,
+	"upvotes" : 10,
+	"downvotes" : 2,
+	"title" : "Neural networks",
+	"summary" : "An overview of neural networks",
+	"block" : 0,
+	"islink" : 1,
+	"link" : "http://neuralnetworksanddeeplearning.com/chap1.html",
+	"time" : ISODate("2018-10-12T17:47:57.933Z")
+}
+{
+	"_id" : ObjectId("5bc22037fdc52ba0ad988d64"),
+	"tag" : "ML",
+	"course" : 1,
+	"upvotes" : 10,
+	"downvotes" : 2,
+	"title" : "Regression",
+	"summary" : "A brief of linear and logistic regression",
+	"block" : 0,
+	"islink" : 1,
+	"link" : "https://www.listendata.com/2014/11/difference-between-linear-regression.html",
+	"time" : ISODate("2018-10-13T16:41:27.899Z")
+}
+{
+	"_id" : ObjectId("5bc220e2fdc52ba0ad988d65"),
+	"tag" : "OOMD",
+	"course" : 1,
+	"upvotes" : 11,
+	"downvotes" : 2,
+	"title" : "OOMD",
+	"summary" : "Unit 2",
+	"block" : 0,
+	"islink" : 1,
+	"link" : "https://drive.google.com/file/d/1QNVuZ1vuKdnwIfRcv1FygFaiNlh6fYLM/view?usp=sharing",
+	"time" : ISODate("2018-10-13T16:44:18.340Z")
+}
+'''
+
 #notes with a particular tag
 @app.route('/notes/<tag>')
 def get_notes(tag):
